cea99dee1ad55dace29f7dcf66d0e9b0d97f210e
Akbar <rosh@MacBook-Air-Batman.local>
2025-05-26 14:05:57 +0500

    debug

--- src/components/layout/navigation-link.tsx
+++ src/components/layout/navigation-link.tsx
@@ -1,50 +1,50 @@
-import { LucideProps } from "lucide-react";
-import { ForwardRefExoticComponent, RefAttributes } from "react";
-import { useTranslation } from "react-i18next";
-import { NavLink } from "react-router-dom";
+// import { LucideProps } from "lucide-react";
+// import { ForwardRefExoticComponent, RefAttributes } from "react";
+// import { useTranslation } from "react-i18next";
+// import { NavLink } from "react-router-dom";
 
-type TProps = {
-    href: string;
-    label: string;
-    Icon?: ForwardRefExoticComponent<
-        Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>
-    >;
-};
+// type TProps = {
+//     href: string;
+//     label: string;
+//     Icon?: ForwardRefExoticComponent<
+//         Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>
+//     >;
+// };
 
-const NavigationLink = ({ href, label, Icon }: TProps) => {
-    const isSame = location.pathname.split("/")[1] === href;
-    const { t } = useTranslation();
-    return (
-        <NavLink
-            to={href}
-            className={(isActive) => {
-                return `flex items-center gap-3 border-primary ${isActive.isActive || isSame
-                    ? "border-l-2 bg-[rgba(0,_138,_62,_0.22)]"
-                    : ""
-                    } px-4 py-2 text-primary transition-all hover:text-primary`;
-            }}
-        >
-            {Icon && (
-                <Icon
-                    size={20}
-                    color={
-                        isSame || location.pathname.split("/")[1] === href.split("/")[1]
-                            ? "#006d2f"
-                            : "#666666"
-                    }
-                />
-            )}
-            <span
-                className={
-                    isSame || location.pathname.split("/")[1] === href.split("/")[1]
-                        ? "text-[#006d2f]"
-                        : "text-[#666666]"
-                }
-            >
-                {t(label)}
-            </span>
-        </NavLink>
-    );
-};
+// const NavigationLink = ({ href, label, Icon }: TProps) => {
+//     const isSame = location.pathname.split("/")[1] === href;
+//     const { t } = useTranslation();
+//     return (
+//         <NavLink
+//             to={href}
+//             className={(isActive) => {
+//                 return `flex items-center gap-3 border-primary ${isActive.isActive || isSame
+//                     ? "border-l-2 bg-[rgba(0,_138,_62,_0.22)]"
+//                     : ""
+//                     } px-4 py-2 text-primary transition-all hover:text-primary`;
+//             }}
+//         >
+//             {Icon && (
+//                 <Icon
+//                     size={20}
+//                     color={
+//                         isSame || location.pathname.split("/")[1] === href.split("/")[1]
+//                             ? "#006d2f"
+//                             : "#666666"
+//                     }
+//                 />
+//             )}
+//             <span
+//                 className={
+//                     isSame || location.pathname.split("/")[1] === href.split("/")[1]
+//                         ? "text-[#006d2f]"
+//                         : "text-[#666666]"
+//                 }
+//             >
+//                 {t(label)}
+//             </span>
+//         </NavLink>
+//     );
+// };
 
-export default NavigationLink;
+// export default NavigationLink;
